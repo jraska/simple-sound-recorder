236d9d86321d8956ff34fe8befbd7da62dfcbfa9
Josef Raska <6277721+jraska@users.noreply.github.com>
2019-02-23 02:16:07 +0100

    MediaPlayer Completable draft

--- app/src/main/java/com/jraska/recorder/recording/RecordsPlayer.kt
+++ app/src/main/java/com/jraska/recorder/recording/RecordsPlayer.kt
@@ -1,6 +1,9 @@
 package com.jraska.recorder.recording
 
 import android.media.MediaPlayer
+import io.reactivex.Completable
+import io.reactivex.CompletableObserver
+import io.reactivex.disposables.Disposable
 import timber.log.Timber
 import java.io.File
 import java.util.*
@@ -27,5 +30,56 @@ class RecordsPlayer @Inject constructor(
         mediaPlayer.start()
 
         mediaPlayer.setOnCompletionListener { mediaPlayer.release() }
+    }
+
+    private class MediaPlayerCompletable(val file: File) : Completable() {
+        override fun subscribeActual(observer: CompletableObserver) {
+            val mediaPlayer = MediaPlayer()
+
+            val disposable = MediaPlayerDisposable(mediaPlayer)
+            observer.onSubscribe(disposable)
+            if (disposable.isDisposed) {
+                return
+            }
+
+            try {
+                mediaPlayer.setDataSource(file.absolutePath)
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+                disposable.playerInitialized
+            } catch (ex: Throwable) {
+                observer.onError(ex)
+                return
+            }
+
+            mediaPlayer.setOnCompletionListener {
+                mediaPlayer.release()
+                observer.onComplete()
+            }
+        }
+    }
+
+    private class MediaPlayerDisposable constructor(
+        private val mediaPlayer: MediaPlayer
+    ) : Disposable {
+        @Volatile
+        private var disposed: Boolean = false
+        var playerInitialized: Boolean = false
+
+        override fun dispose() {
+            if (disposed) {
+                return
+            }
+            disposed = true
+
+            if (playerInitialized) {
+                mediaPlayer.pause()
+                mediaPlayer.stop()
+            }
+        }
+
+        override fun isDisposed(): Boolean {
+            return disposed
+        }
     }
 }
